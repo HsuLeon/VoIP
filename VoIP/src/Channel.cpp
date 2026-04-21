268e1c8ec4c627ad3e192647ba80e49938b6bcbc
Leon.Hsu <leon0718@gmail.com>
2026-04-21 16:54:38 +0800

    modify AES time

--- VoIP/src/Channel.cpp
+++ VoIP/src/Channel.cpp
@@ -276,7 +276,7 @@ struct Channel::Impl {
         shutdownEchoCancel();
         if (!config.enableEchoCancel) return true;
 
-        constexpr int FILTER_MS = 200;
+        constexpr int FILTER_MS = 120;
         const int filterLength = SAMPLE_RATE * FILTER_MS / 1000;
 
         echoState = speex_echo_state_init_mc(
@@ -297,8 +297,8 @@ struct Channel::Impl {
         int denoise = 0;
         int vad = 0;
         int agc = 0;
-        int echoSuppress = -40;
-        int echoSuppressActive = -15;
+        int echoSuppress = -55;
+        int echoSuppressActive = -20;
         speex_preprocess_ctl(
             echoPreprocess, SPEEX_PREPROCESS_SET_DENOISE, &denoise);
         speex_preprocess_ctl(
